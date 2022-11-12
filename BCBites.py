9abf288c1a3ce6010c741bef0d06f71e17cdc6bf
seminyoon1 <seminyoon1@gmail.com>
2022-11-12 14:12:49 -0500

    added in files from other

--- BCBites.py
+++ BCBites.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("home.html", Item1 = "hi", Item2 = "ih")
 
 if __name__ == '__main__':
    app.run(debug=True)
