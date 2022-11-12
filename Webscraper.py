a5c8f97a79b42ac29792a5b54f6cbfab23903557
seminyoon1 <seminyoon1@gmail.com>
2022-11-12 17:25:03 -0500

    updated webscraper

--- Webscraper.py
+++ Webscraper.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 diningHalls = {"Lower Live": 21, "Carney's ": 23, "Stuart Hall": 28}
-def getDiningHall(name):
+def getURL(name):
     forbiddenNumbers = []
     if name == "Lower Live":
         forbiddenNumbers = ["95", "41", "28", "04", "09", "27", "05", "97", "31"]
@@ -10,18 +10,16 @@ def getDiningHall(name):
         forbiddenNumbers = ["47", "49", "95", "97", "05", "09", "50"]
     elif name == "Stuart Hall":
         forbiddenNumbers = ["95", "07", "05", "15", "09", "08"]
-    else:
-        return "Not a valid Dining Hall"
     webUrl = "https://web.bc.edu/dining/menu/todayMenu_PROD.json"
     allList = json.loads(requests.get(webUrl).text)
     diningList = []
     for i in range(len(allList)):
         if allList[i]["Location_Name"] == name:
             if allList[i]["Menu_Category_Number"] not in forbiddenNumbers and allList[i]["Selling_Price"][:-2] != ".00":
-                diningList.append([allList[i]["Meal_Name"], allList[i]["Recipe_Print_As_Name"], allList[i]["Selling_Price"][:-2]])
-    return diningList
+                diningList.append([allList[i]["Meal_Name"], allList[i]["Menu_Category_Number"], allList[i]["Menu_Category_Name"], allList[i]["Recipe_Print_As_Name"], allList[i]["Selling_Price"][:-2]])
+    return json.dumps(diningList)
 
 
-print(getDiningHall("Stuart Hall"))
+print(getURL("Lower Live"))
 
 
