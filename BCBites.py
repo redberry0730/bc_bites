14b7129b1c4ce488b60168a3974adfc450f08b32
seminyoon1 <seminyoon1@gmail.com>
2022-11-12 20:24:51 -0500

    added method to get and post

--- BCBites.py
+++ BCBites.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/') 
+@app.route('/', methods=['GET', 'POST']) 
 def index():
+    if request.method == 'POST':
+        return redirect('vote')
     return render_template("home.html")
+
+@app.route('/vote', methods=['GET', 'POST']) 
+def vote():
+    return "Hi"
 
 if __name__ == '__main__':
    app.run(debug=True)
