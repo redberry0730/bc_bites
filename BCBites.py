adf918bc59f6fbc458479215d8f6005469a4c4f0
seminyoon1 <seminyoon1@gmail.com>
2022-11-12 22:20:29 -0500

    added functionality

--- BCBites.py
+++ BCBites.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST']) 
 def index():
     if request.method == 'POST':
-        return redirect('vote')
+        return redirect('/carney')
     return render_template("home.html")
 
-@app.route('/vote', methods=['GET', 'POST']) 
-def vote():
-    return "Hi"
+@app.route('/carney', methods=['GET', 'POST']) 
+def carney():
+    return render_template("Carney.html")
+    
+@app.route('/lower', methods=['GET', 'POST']) 
+def lower():
+    return render_template("Lower.html")
+
+@app.route('/stuart', methods=['GET', 'POST']) 
+def carney():
+    return render_template("Stuart.html")
 
 if __name__ == '__main__':
    app.run(debug=True)
